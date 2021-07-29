cb4e4ec5c02439341368759a61f9e8fadb4e1a4a
jack4014 <jack4014@hotmail.co.nz>
2021-07-30 11:12:07 +1200

    Add project files.

---
+++ Retrieve from AccessDB/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace Retrieve_from_AccessDB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StartButton = new System.Windows.Forms.Button();
+            this.QueryBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(329, 225);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(107, 36);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // QueryBox
+            // 
+            this.QueryBox.Location = new System.Drawing.Point(252, 109);
+            this.QueryBox.Name = "QueryBox";
+            this.QueryBox.Size = new System.Drawing.Size(250, 74);
+            this.QueryBox.TabIndex = 1;
+            this.QueryBox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.QueryBox);
+            this.Controls.Add(this.StartButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.RichTextBox QueryBox;
+    }
+}
+
---
+++ Retrieve from AccessDB/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Retrieve_from_AccessDB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            String ConnStr = @"Provider=Microsoft.ACE.OLEDB.12.0; Data Source =
+                             C:\\Users\\Jack\\Downloads\\FarmInfomation.accdb;
+                             Persist Security Info = False";
+            String err_Msg = "";
+            String q = "";
+            OleDbConnection conn = null;
+            try
+            {
+                conn = new OleDbConnection(ConnStr);
+                conn.Open();
+                q = QueryBox.Text;
+                OleDbCommand cmd = new OleDbCommand(q, conn);
+                String str = "";
+                using (OleDbDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        str = str + reader["ID"].ToString() + "\r\n";
+                    }
+                    MessageBox.Show(str);
+                }
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+               MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
